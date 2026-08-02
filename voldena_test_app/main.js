d28586fa102a1588c80722c86b992aaf765fc143
Yavuz Efe Kızmaz <efescsus@gmail.com>
2026-08-03 01:40:57 +0300

    voldena_test_app registry uninstall list trace added

--- voldena_test_app/main.js
+++ voldena_test_app/main.js
@@ -17,10 +17,24 @@ function createWindow() {
 
 function leaveTraces() {
     const appName = "voldena_test_app";
+    const installDir = app.getAppPath();
+    const escapedInstallDir = installDir.replace(/\\/g, '\\\\');
     
     const regCmd = [
         `$regPath = 'HKCU:\\Software\\${appName}'`,
         `if (-not (Test-Path $regPath)) { New-Item -Path $regPath -Force | Out-Null; New-ItemProperty -Path $regPath -Name 'DummyData' -Value 'I am a trace!' -PropertyType String -Force | Out-Null }`,
+        
+        // Add to Uninstall list so it shows up in Voldena Uninstaller
+        `$uninstallRegPath = 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\${appName}'`,
+        `if (-not (Test-Path $uninstallRegPath)) {`,
+        `    New-Item -Path $uninstallRegPath -Force | Out-Null`,
+        `    New-ItemProperty -Path $uninstallRegPath -Name 'DisplayName' -Value 'Voldena Test App' -PropertyType String -Force | Out-Null`,
+        `    New-ItemProperty -Path $uninstallRegPath -Name 'UninstallString' -Value 'powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ''Remove-Item -Path HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\${appName} -Force; Remove-Item -Path HKCU:\\Software\\${appName} -Force''' -PropertyType String -Force | Out-Null`,
+        `    New-ItemProperty -Path $uninstallRegPath -Name 'DisplayVersion' -Value '1.0.0' -PropertyType String -Force | Out-Null`,
+        `    New-ItemProperty -Path $uninstallRegPath -Name 'Publisher' -Value 'Voldena' -PropertyType String -Force | Out-Null`,
+        `    New-ItemProperty -Path $uninstallRegPath -Name 'InstallLocation' -Value '${escapedInstallDir}' -PropertyType String -Force | Out-Null`,
+        `}`,
+        
         `$appDataDir = Join-Path $env:APPDATA '${appName}'`,
         `if (-not (Test-Path $appDataDir)) { New-Item -ItemType Directory -Force -Path $appDataDir | Out-Null }`,
         `Set-Content -Path (Join-Path $appDataDir 'trace.log') -Value 'This is a garbage log file left by the test app.'`,
